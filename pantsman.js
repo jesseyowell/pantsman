bf68f3768ce6221c62285cbfbe18afa35dab53f1
Jesse Yowell <jesse.yowell@gmail.com>
2018-05-15 21:47:44 -0700

    added config file and added error handling

--- pantsman.js
+++ pantsman.js
@@ -1,20 +1,25 @@
 // This part is from davidwash.name/nodejs-irc
 
-var irc = require("irc");
+var irc = require('irc');
 
-var config = require("./config");
+var config = require('config');
 
-var bot = new irc.Client(config.server, config.botName, {
-	channels: config.channels
-});
+var bot = new irc.Client(config.server, config.botName, config.channels);
+
+//var bot = new irc.Client('irc.esper.net', 'pantsman', {
+//    channels: ['#fishmongers'],
+//});
 
 // expand this to markov functionality
 
+bot.addListener('message', function (from, to, message) {
+    console.log(from + ' => ' + to + ': ' + message);
+});
 
-bot.addListener("connect", function() {
-	bot.handleArrival("connect");
-})
-
-bot.addListener("message", function(from, to, text, message) {
+bot.addListener('message', function(from, to, text, message) {
 	bot.say(config.channels[0], "wat");
+});
+
+bot.addListener('error', function(message) {
+    console.log('error: ', message);
 });
