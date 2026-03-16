05f7b2bd6c4dbdb907b73bfe75a84ef11ec862b9
jesse <jesse.yowell@gmail.com>
2026-03-16 16:37:09 -0700

    feat: wire markov chain into irc bot (train on messages, reply by name or random chance)

--- pantsman.js
+++ pantsman.js
@@ -1,21 +1,45 @@
-// This part is from davidwash.name/nodejs-irc
-
 var irc = require('irc');
 var config = require('config');
+var markov = require('./markov');
+
+markov.load(config.corpusFile);
+
 var bot = new irc.Client(config.server, config.botName, {
-	channels: config.channels
+    channels: config.channels
 });
 
-// expand this to markov functionality
+bot.addListener('message', function(from, to, message, raw) {
+    // ignore DMs (to === our nick)
+    if (to.toLowerCase() === config.botName.toLowerCase()) return;
+    // ignore our own messages
+    if (from.toLowerCase() === config.botName.toLowerCase()) return;
 
-bot.addListener('message', function (from, to, message) {
-    console.log(from + ' => ' + to + ': ' + message);
+    markov.train(message);
+
+    var reply = null;
+    if (message.toLowerCase().indexOf(config.botName.toLowerCase()) !== -1) {
+        reply = markov.generate(config.maxWords);
+    } else if (Math.random() < config.replyChance) {
+        reply = markov.generate(config.maxWords);
+    }
+
+    if (reply !== null) {
+        bot.say(to, reply);
+    }
 });
 
-bot.addListener('message', function(from, to, text, message) {
-	bot.say(config.channels[0], "wat");
+bot.addListener('nick', function(message, to, from) {
+    bot.say(config.channels[0], "wat");
 });
 
 bot.addListener('error', function(message) {
     console.log('error: ', message);
 });
+
+function shutdown() {
+    markov.save(config.corpusFile);
+    process.exit(0);
+}
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
